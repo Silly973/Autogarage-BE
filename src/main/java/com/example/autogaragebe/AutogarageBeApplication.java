945dbd019a34aff4c2ee2501578fe5936dc141ac
Silly973 <yogabeertje@gmail.com>
2023-01-27 10:06:57 +0100

    first coommit

---
+++ src/main/java/com/example/autogaragebe/AutogarageBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.autogaragebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogarageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutogarageBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autogaragebe/AutogarageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autogaragebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutogarageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
